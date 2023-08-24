ab1b2691f599a2c218357eb987781e33549c2756
xiaofanc-gatech <xiaofanc@gatech.edu>
2023-08-23 20:41:58 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import About from "./components/About";
+import Navbar from "./components/Navbar";
+import Projects from "./components/Projects";
+import Skills from "./components/Skills";
+import Contact from "./components/Contact";
+
+export default function App() {
+  return (
+    <main className="text-gray-400 bg-gray-900 body-font">
+      <Navbar />
+      <About />
+      <Projects />
+      <Skills />
+      <Contact />
+    </main>
+  );
+}
