8c736f34c20367c6de45b21c84c02ac988bd0ad7
xiaofanc-gatech <xiaofanc@gatech.edu>
2023-08-24 15:11:20 -0700

    update contact form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -19,7 +19,7 @@ export default function Contact() {
         fetch("/", {
             method: "POST",
             headers: { "Content-Type": "application/x-www-form-urlencoded" },
-            body: encode({ "form-name": "contact", name, email, message }),
+            body: encode({ "form-name": "contactMe", name, email, message }),
         })
             .then(() => alert("Message sent!"))
             .catch((error) => alert(error));
@@ -65,7 +65,7 @@ export default function Contact() {
                 </div>
                 <form
                     netlify
-                    name="contact"
+                    name="contactMe"
                     onSubmit={handleSubmit}
                     className="lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
                     <h2 className="text-white sm:text-4xl text-3xl mb-1 font-medium title-font">
