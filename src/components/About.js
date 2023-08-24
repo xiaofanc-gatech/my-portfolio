34443510fc9c56e2a5c7232998eec7ae15606045
xiaofanc-gatech <xiaofanc@gatech.edu>
2023-08-23 21:03:47 -0700

    update about

--- src/components/About.js
+++ src/components/About.js
@@ -11,9 +11,7 @@ export default function About() {
                         apps.
                     </h1>
                     <p className="mb-8 leading-relaxed">
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui
-                        laborum quasi, incidunt dolore iste nostrum cupiditate voluptas?
-                        Laborum, voluptas natus?
+                        I have a good understanding of both software and data with 2+ years’ working experience in data analysis. Currently I am studying in the Master of Science in Computer Science program at Georgia Tech. I am passionate about developing interesting software products and solving challenging problems. Currently looking for Software Engineer roles.
                     </p>
                     <div className="flex justify-center">
                         <a
